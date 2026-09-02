48ff72530af5600955a72e0174880651661de859
Ella Braun <ellafbraun@gmail.com>
2026-09-02 18:50:55 +0100

    add key press event to stop drawing

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const container = document.querySelector("#container");
 const controls = document.querySelector("#controls")
 const title = document.createElement("h1")
 title.classList.add("title");
+const instruction = document.createElement("p")
+instruction.classList.add("info")
 const refresh = document.createElement("button");
 refresh.classList.add("refresh");
 refresh.textContent = "Refresh Grid";
 title.textContent = "Ella's Etch-a-Sketch";
+instruction.textContent = "Press the D key to stop drawing, press it again to begin drawing";
 controls.appendChild(title);
+controls.appendChild(instruction);
 controls.appendChild(refresh);
 
+let drawing = true;
+document.addEventListener("keydown", (event) => {//event being passed in will tell us more about the event
+    if (event.key === "d") {
+        drawing = !drawing;//this will switch the drawing var no matter what it starts with
+    }
+});
 
 refresh.addEventListener ("click", () => {
     const squares = document.querySelectorAll(".square");
@@ -18,12 +28,13 @@ refresh.addEventListener ("click", () => {
     });
 });
 
-
 for(i=0; i<1600; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     square.addEventListener("mouseenter", () => {
-        square.classList.add("hover");
+        if (drawing) {
+            square.classList.add("hover");
+        }
     });
     // square.addEventListener("mouseleave", () => {
     //     square.classList.remove("hover");
