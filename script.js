d3744b390da2de35e8e566fc51e0518d93ad4933
Ella Braun <ellafbraun@gmail.com>
2026-09-02 18:31:23 +0100

    adding writing capability and clearing

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const container = document.querySelector("#container");
+const controls = document.querySelector("#controls")
+const title = document.createElement("h1")
+title.classList.add("title");
+const refresh = document.createElement("button");
+refresh.classList.add("refresh");
+refresh.textContent = "Refresh Grid";
+title.textContent = "Ella's Etch-a-Sketch";
+controls.appendChild(title);
+controls.appendChild(refresh);
 
-for(i=0; i<256; i++) {
+
+refresh.addEventListener ("click", () => {
+    const squares = document.querySelectorAll(".square");
+    //this contains every square, so we don't have to loop through all
+    squares.forEach(square => { //for each square in squares
+    square.classList.remove("hover");
+    });
+});
+
+
+for(i=0; i<1600; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("hover");
+    });
+    // square.addEventListener("mouseleave", () => {
+    //     square.classList.remove("hover");
+    // }); 
     container.appendChild(square);  
 }
